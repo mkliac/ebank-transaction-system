6722a135884c657433d8efee5f13df43b6fb6f7f
mkliac <90816129+mkliac@users.noreply.github.com>
2023-04-09 16:27:57 +0800

    add test file

---
+++ src/test/java/com/example/ebanktransactionsystem/service/ExchangeServiceTest.java
@@ -0,0 +1,15 @@
+package com.example.ebanktransactionsystem.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ExchangeServiceTest {
+
+    @Test
+    void getDebitsAndCreditsFromTransactions() {
+
+    }
+}
