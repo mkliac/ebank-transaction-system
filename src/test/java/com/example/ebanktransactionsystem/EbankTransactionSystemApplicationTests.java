e56f9bfcca68f9bf9f560f34bf5d028e9da5a85c
mkliac <90816129+mkliac@users.noreply.github.com>
2023-04-09 16:19:52 +0800

    hide test file

--- src/test/java/com/example/ebanktransactionsystem/EbankTransactionSystemApplicationTests.java
+++ src/test/java/com/example/ebanktransactionsystem/EbankTransactionSystemApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.ebanktransactionsystem;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EbankTransactionSystemApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.ebanktransactionsystem;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EbankTransactionSystemApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
